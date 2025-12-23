bf99230cfb05dcb08f65bbca361cbec9425a26f8
mariannebarbin-png <mariannebarbin@gmail.com>
2025-12-23 13:20:59 +0800

    final

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,17 +33,14 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast. ensures user are secure/compatible with laravel
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string,string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 
     /**
      * A user can have many marine collections
